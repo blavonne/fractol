4d98f91b10e69441bf38db3cc900018a798ec2c2
blavonne <luchik_sonza@mail.ru>
2020-10-22 09:13:37 +0300

    change move mode

--- srcs/move.c
+++ srcs/move.c
@@ -32,14 +32,13 @@
 void		move_a(int key, t_fractol *fractol)
 {
 	if (key == ARROW_RIGHT)
-		fractol->a.min.re -= 0.005;
+		fractol->a.min.re -= 0.015 * fractol->a.size;
 	else if (key == ARROW_LEFT)
-		fractol->a.min.re += 0.005;
+		fractol->a.min.re += 0.015 * fractol->a.size;
 	else if (key == ARROW_DOWN)
-		fractol->a.min.im -= 0.005;
+		fractol->a.min.im -= 0.015 * fractol->a.size;
 	else if (key == ARROW_UP)
-		fractol->a.min.im += 0.005;
-//	fractol->a.draw_algebraic[fractol->a.type](fractol);
+		fractol->a.min.im += 0.015 * fractol->a.size;
 }
 
 void		move_g(int key, t_fractol *fractol)
